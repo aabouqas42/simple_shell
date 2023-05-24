88b2fd0e45cdb34aa1aae764c25a23aa1dc4f860
AissamDEV <aissamaboukassim@gmail.com>
2023-05-24 10:46:25 -0700

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,84 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 
-#define MAX_COMMAND_LENGTH 100
 /**
- * execute_command - Executes a command in a child process
+ * print_prompt - Prints the shell prompt
+ */
+void print_prompt(void)
+{
+	printf("#cisfun$ ");
+}
+
+/**
+ * read_line - Reads a line of input from the user
+ * Return: The input line as a string
+ */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t characters_read;
+
+	characters_read = getline(&line, &bufsize, stdin);
+	if (characters_read == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		} else
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return (line);
+}
+
+/**
+ * execute_command - Executes a command
  * @command: The command to execute
  */
 void execute_command(char *command)
 {
-	pid_t pid = fork();
+	int status;
+	pid_t child_pid;
+	char **argv = NULL;
+	char *envp[] = {NULL};
 
-	if (pid < 0)
+	argv = malloc(2 * sizeof(char *));
+	if (argv == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	argv[0] = command;
+	argv[1] = NULL;
+
+	child_pid = fork();
+	if (child_pid == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (pid == 0)
+	} else if (child_pid == 0)
 	{
-		char *args[MAX_COMMAND_LENGTH];
-		int i;
-
-		args[0] = strtok(command, " ");
-		i = 1;
-
-		while (args[i - 1] != NULL && i < MAX_COMMAND_LENGTH)
-		{
-			args[i] = strtok(NULL, " ");
-			i++;
-		}
-
-		if (execve(args[0], args, NULL) == -1)
+		if (execve(command, argv, envp) == -1)
 		{
 			perror("execve");
 			exit(EXIT_FAILURE);
 		}
 	} else
 	{
-		int status;
-
-		waitpid(pid, &status, 0);
-
-		if (status != 0)
-		{
-			printf("%s: command not found\n", command);
-		}
+		waitpid(child_pid, &status, 0);
 	}
+	free(argv);
 }
+
 /**
- * run_shell - Runs the UNIX command line shell
+ * main - Program that executes a UNIX command line interpreter
+ * Return: Always 0
  */
-void run_shell(void)
+int main(void)
 {
-	char command[MAX_COMMAND_LENGTH];
+	char command[100];
 
 	while (1)
 	{
 		printf("#cisfun$ ");
-		fflush(stdout);
-
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			printf("\n");
-			break;
-		}
+		fgets(command, sizeof(command), stdin);
 
 		command[strcspn(command, "\n")] = '\0';
 
 		execute_command(command);
 	}
-}
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	run_shell();
-	return (EXIT_SUCCESS);
+	return (0);
 }
